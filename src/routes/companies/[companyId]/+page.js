bbd29f118e848356331f769b9212ae3509c685d9
lauyoty14 <octavio.pastorive@gmail.com>
2024-08-07 17:13:44 -0300

    solucion compania sin productos

--- src/routes/companies/[companyId]/+page.js
+++ src/routes/companies/[companyId]/+page.js
@@ -1,44 +1,48 @@
 // @ts-ignore
 export const load = async ({ params, fetch }) => {
-    //console.log('Params:', params);
-    
+    console.log('Params:', params);
+
     // @ts-ignore
     const fetchProduct = async (companyID) => {
         const res = await fetch(`http://localhost:8081/v1/products/${companyID}/admin`);
         if (!res.ok) {
+            if (res.status === 404) {
+                console.log('No products found');
+                return []; // Retorna un array vacío si no se encuentran productos
+            }
             throw new Error(`Failed to fetch products: ${res.statusText}`);
         }
-        const productData = res.json();
-        return productData
+        const productData = await res.json();
+        return productData;
     };
+
     // @ts-ignore
     const fetchCompany = async (companyID) => {
-      const res = await fetch(`http://localhost:8081/v1/clients/${companyID}`);
-      if (!res.ok) {
-          throw new Error(`Failed to fetch company: ${res.statusText}`);
-      }
-      const companyData = res.json();
-      return companyData
-  };
+        const res = await fetch(`http://localhost:8081/v1/clients/${companyID}`);
+        if (!res.ok) {
+            throw new Error(`Failed to fetch company: ${res.statusText}`);
+        }
+        const companyData = await res.json();
+        return companyData;
+    };
 
     try {
+        const company = await fetchCompany(params.companyId);
         const products = await fetchProduct(params.companyId);
-        const company = await fetchCompany(params.companyId)
         const companyID = params.companyId;
-        //console.log('compania:',company)
-        return {
-            products, 
-            companyID,
-            company
+
+        return { 
+                products,
+                companyID,
+                company
+        
         };
     } catch (error) {
-        console.error(error);
+        console.error('Error:', error);
         return {
-            props: {
                 products: [], // En caso de error, aseguramos que sea un array
                 companyID: params.companyId,
-                company: {}
-            }
+                company: {} // Retorna un objeto vacío en caso de error
         };
     }
-}
+};
